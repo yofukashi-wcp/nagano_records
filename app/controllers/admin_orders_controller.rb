50fdf4455d0b316e0c43f1cd93ec04dce9bbc652
aki-utchy <akihiro.uchida.w@gmail.com>
2019-08-20 15:02:05 +0900

    first commit 70%

--- app/controllers/admin_orders_controller.rb
+++ app/controllers/admin_orders_controller.rb
@@ -1,11 +1,13 @@
 class AdminOrdersController < ApplicationController
 
     def index
+        @orders = Order.all
 
     end
 
     def show
-
+        @order = Order.find(params[:id])
+        @order_products = @order.order_products.all
     end
 
     def update
--- app/helpers/admin_orders_helper.rb
+++ app/helpers/admin_orders_helper.rb
@@ -1,2 +1,9 @@
 module AdminOrdersHelper
+    def sum_quantity(orderproducts)
+        sum= 0
+        orderproducts.each do |orderproduct|
+        sum += orderproduct.quantity
+        end
+        sum
+    end
 end
