f69004b826d6691d3cd331e4c3eb14cb0d88c7bc
Taiga Yoshizaki <taiga@Taiga-macbook.local>
2019-08-26 16:40:05 +0900

    add bootstrap.class to table

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,17 @@
 class CartsController < ApplicationController
     def index
         @carts = Cart.all
+        @products = Product.all
         @total_price = @products.sum(:price)
+        @cart = Cart.find_by(params[:id])
+        unless @cart then
+        end
     end
 
     def create
-
+        cart = Cart.new(product_params)
+        cart.user_id = current_user.id
+        cart.save!
     end
 
     def update
@@ -13,6 +19,14 @@ class CartsController < ApplicationController
     end
 
     def destroy
+        @cart.destroy
+        redirect_to carts_path
+    end
 
+
+
+    private
+    def product_params
+    params.require(:cart).permit(:product_id, :quantity)
     end
 end
