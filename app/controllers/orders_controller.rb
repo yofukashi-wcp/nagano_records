e1e9742539b04f19ce8b3b6a2ba770b3fbcaf84b
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-08-26 19:07:40 +0900

    orders new

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,9 @@ class OrdersController < ApplicationController
     end
 
     def new
-        @user = current_user
-        @cart = Cart.find(params[:id])
+        @carts = Cart.where(user_id: current_user.id)
+        cart_total_price(@carts)
+        
         
     end
 
@@ -16,5 +17,18 @@ class OrdersController < ApplicationController
 
     def update
 
+    end
+
+    private
+
+    def order_params
+        params.require(:cart).permit(:product_id, :quantity)
+    end
+    def cart_total_price(carts)
+        @cart_total_price = 0
+        carts.each do |cart|
+            cart_subtotal_price = cart.quantity * cart.product.price
+            @cart_total_price += cart_subtotal_price
+        end
     end
 end
