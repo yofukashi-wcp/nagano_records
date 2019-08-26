b552fa8e0d5f301238388f4029893f2f18006add
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-08-26 16:04:06 +0900

    orders new

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
 class OrdersController < ApplicationController
 
     def index
-
+        @user = User.find(params[:id])
     end
 
     def new
-
+        @user = current_user
+        @cart = Cart.find(params[:id])
+        
     end
 
     def check
