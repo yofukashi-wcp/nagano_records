c660c682739853cda6f65c727eb2a39934be3493
aki-utchy <akihiro.uchida.w@gmail.com>
2019-08-28 19:43:20 +0900

    commit 20190828_1945

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -65,7 +65,7 @@ class OrdersController < ApplicationController
     end
 
     def check 
-        @order = current_user.orders.order(created_at: "DESC").last
+        @order = current_user.orders.order(created_at: "ASC").last
     end
 
     def update
